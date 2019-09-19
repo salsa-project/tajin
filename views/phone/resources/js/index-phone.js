2a7719592483b88d46bf0174ddf7c3d7203dcfda
salsa-project <intenso524@gmail.com>
2019-09-19 04:18:45 +0100

    DONE-> Restyle phone navbar icons

--- views/phone/resources/js/index-phone.js
+++ views/phone/resources/js/index-phone.js
@@ -2,7 +2,12 @@ const navItems = document.getElementsByClassName('hrefItem');
 Array.from(navItems).forEach(function(item){
   let href = window.location.href;
   if(href.indexOf(item.getAttribute('href')) != -1){
-    item.style.borderBottom = "2px #fc4f00 solid";
-    //item.getAttribute('href')
+  }else{
+    item.style = `filter: grayscale(1);`;
   }
 })
+let url = window.location.href;
+const home = document.getElementById('home');
+if(url.indexOf('/home') == -1){
+  home.style = `filter: grayscale(1);`;
+}
