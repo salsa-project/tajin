6c3f3bec2b836174bd49846e6d7c88c9523b46bb
salsa-project <intenso524@gmail.com>
2019-09-21 06:55:45 +0100

    DONE-> PHONE-> Style login page

--- views/resources/js/index.js
+++
@@ -1,8 +0,0 @@
-const navItems = document.getElementsByClassName('hrefItem');
-Array.from(navItems).forEach(function(item){
-  let href = window.location.href;
-  if(href.indexOf(item.getAttribute('href')) != -1){
-    item.style.borderBottom = "2px #fc4f00 solid";
-    //item.getAttribute('href')
-  }
-})
